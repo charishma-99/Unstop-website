863bcffc16735c10ef3ad9dd13bbc1cd5fbe02c4
Chidiebere Ibiam <86835158+ChidiebereIbiam@users.noreply.github.com>
2023-03-15 11:09:38 +0100

    Completed User Registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import UserSignupForm
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import UserProfile
 
 # Create your views here.
+
+class UserRegisterView(generic.CreateView):
+    form_class = UserSignupForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
