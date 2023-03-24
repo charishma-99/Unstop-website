e2b038e7d5fb5fb894152dbab959ae8f67658eda
Chidiebere Ibiam <86835158+ChidiebereIbiam@users.noreply.github.com>
2023-03-24 10:25:02 +0100

    Completed Home Page Design

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('job-portal/',views.job_portal,name='job_portal'),
+    path('',views.job_portal,name='job_portal'),
 ]
--- unstop/urls.py
+++ unstop/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('courses/', include('courses.urls')),
+    path('jobs/', include('jobs.urls')),
 ]  + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
