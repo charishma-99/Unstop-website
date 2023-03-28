8a23e2550f5cb6b49eb8a93428c7a7e70021a603
Chidiebere Ibiam <86835158+ChidiebereIbiam@users.noreply.github.com>
2023-03-28 13:25:25 +0100

    Started Frontend Design of Learn Page

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('add-course-content/', views.add_course_content, name="add_course_content"),
     path('add-section/', views.add_section, name="add_section"),
     path('add-course/', views.add_course, name="add_course"),
-    path('learn/', views.learn, name="learn"),
+    path('', views.learn, name="learn"),
     path('course-detail/<int:id>', views.course_detail, name="course_detail"),
 ]
