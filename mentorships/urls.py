acaa9d3eb4f05ee9df7a6cc3ff072006d5b0275c
Henry <hogbunike@gmail.com>
2023-04-27 16:32:21 +0100

    started views for mentor

--- mentorships/urls.py
+++ mentorships/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('mentors', views.mentors, name='mentors'),
+    
 ]
--- mentorships/views.py
+++ mentorships/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Mentor
+from accounts.views import ShowProfile
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def mentors(request):
+    mentors = Mentor.objects.all()
+    context = {
+        'mentors': mentors,
+    
+    }
+    return render(request, 'mentors.html', context)
