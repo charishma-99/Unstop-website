cce1b783e0b3c4bc177e5ea684ced49d748aad97
Kunal <jaink7069@gmail.com>
2023-04-29 12:49:20 +0530

    filters on mentorship page are working

--- mentorships/views.py
+++ mentorships/views.py
@@ -8,8 +8,16 @@ def home(request):
 
 def mentors(request):
     mentors = Mentor.objects.all()
+    id= request.GET.get('id')
+    if id=='1':
+        mentors = Mentor.objects.filter(mentor_type= 'technology')
+    elif id=='2':
+        mentors = Mentor.objects.filter(mentor_type= 'management')
+    elif id=='3':
+        mentors = Mentor.objects.all() 
+    else:
+      mentors = Mentor.objects.all()  
     context = {
         'mentors': mentors,
-    
     }
     return render(request, 'mentors.html', context)
