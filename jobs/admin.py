4616d0a6c183e5367f3df5d998e81bd0fe3bb80d
gokul <ghasy79>
2023-05-04 22:22:10 +0530

    Created Django Admmin site

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from jobs.models import Job
 # Register your models here.
+
+class JobAdmin(admin.ModelAdmin):
+    list = ['title', 'organization', 'description', 'application_deadline', 'location', 'experience', 'salary', 'work_details', 'job_type']
+
+admin.site.register(Job, JobAdmin)
