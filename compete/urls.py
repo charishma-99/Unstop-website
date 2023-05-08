b72e910cb529e62fee805d1a9c1b0143b3421242
manan <manan9950678721@gmail.com>
2023-05-08 22:04:27 +0530

    changes made

--- compete/urls.py
+++ compete/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.compete_portal,name='compete_portal'),
+    path('',views.Compete,name='compete'),
 ]
--- compete/views.py
+++ compete/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def compete_portal(request):
+def Compete(request):
     return render(request,'compete.html')
